e54a986211d528c6fe6457f0f5a96bd50e97d738
Shin Jihoon <tobeabestns@naver.com>
2025-07-15 01:47:46 +0900

    refactor: theme, globalstyle을 App.jsx -> main.jsx 로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import { lightTheme, darkTheme, GlobalStyle } from "./App.styled.js";
-import { ThemeProvider } from "styled-components";
 import { Routes, Route } from "react-router-dom";
 import Gnb from "./components/headers/Gnb";
 import NewsListPage from "./pages/newsListPage";
-import useThemeStore from "./stores/themeStore.js";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function App() {
-  const { theme } = useThemeStore();
-  const currentTheme = theme === "light" ? lightTheme : darkTheme;
 
   return (
     <>
-      <ThemeProvider theme={currentTheme}>
-        <GlobalStyle />
-        <Gnb />
-        <main>
-          <Routes>
-            <Route path="/" element={<NewsListPage />} />
-            <Route path="/news/:category" element={<NewsListPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </main>
-      </ThemeProvider>
+      <Gnb />
+      <main>
+        <Routes>
+          <Route path="/" element={<NewsListPage />} />
+          <Route path="/news/:category" element={<NewsListPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </>
   );
 }
